cd437cc4a33e6c59f286b1f8c247ee444cd14983
Kristof Lißmann <lissmann@email.de>
2024-12-30 19:03:20 +0100

    Improve `Ball` logic with direction, speed, and collision handling
    Refactored `Ball` class to include direction and speed properties for better control. Added collision handling to restrict movement within the canvas boundaries. Updated `Pong` initialization to include a configurable ball speed.

--- public/assets/js/pong/Ball.js
+++ public/assets/js/pong/Ball.js
@@ -1,17 +1,79 @@
 import Drawable from "./Drawable.js";
 
 export default class Ball extends Drawable {
-    constructor(context, x, y, diameter, color) {
+    #directionX;
+    #directionY;
+    #speed;
+
+    constructor(context, x, y, diameter, color, speed) {
         super(context, x, y, diameter / 2, diameter / 2, color);
+
+        this.#speed = speed;
+        this.#directionX = speed;
+        this.#directionY = speed;
     }
 
     draw() {
         super.context.fillStyle = super.color;
 
+        const radius = super.width;
+        const newX = super.x + this.#directionX;
+        const leftEdge = newX - radius;
+        const rightEdge = newX + radius;
+        const canvasWidth = super.context.canvas.width;
+        const maximumRightEdge = canvasWidth - radius;
+
+        if (leftEdge > 0 && rightEdge < canvasWidth) {
+            super.x += this.#directionX;
+        } else if (leftEdge <= 0) {
+            super.x = radius;
+            this.right();
+        } else if (rightEdge >= maximumRightEdge) {
+            super.x = maximumRightEdge;
+            this.left();
+        }
+
+        const newY = super.y + this.#directionY;
+        const topEdge = newY - radius;
+        const bottomEdge = newY + radius;
+        const canvasHeight = super.context.canvas.height;
+        const maximumBottomEdge = canvasHeight - radius;
+
+        if (topEdge > 0 && bottomEdge < canvasHeight) {
+            super.y += this.#directionY;
+        } else if (topEdge <= 0) {
+            super.y = radius;
+            this.down();
+        } else if (bottomEdge >= maximumBottomEdge) {
+            super.y = maximumBottomEdge;
+            this.up();
+        }
+
         super.context.beginPath();
-        super.context.arc(super.x, super.y, 10, 0, Math.PI * 2);
+        super.context.arc(super.x, super.y, 10, 0, 2 * Math.PI);
         super.context.fill();
         super.context.closePath();
+    }
+
+    left() {
+        this.#directionX = -this.#speed;
+    }
+
+    right() {
+        this.#directionX = this.#speed;
+    }
+
+    up() {
+        this.#directionY = -this.#speed;
+    }
+
+    down() {
+        this.#directionY = this.#speed;
+    }
+
+    idle() {
+        this.#directionX = 0;
+        this.#directionY = 0;
     }
 
     set context(context) {
@@ -24,5 +86,13 @@ export default class Ball extends Drawable {
     }
 
     set color(color) {
+    }
+
+    get speed() {
+        return this.#speed;
+    }
+
+    set speed(speed) {
+        this.#speed = speed;
     }
 }
--- public/assets/js/pong/Pong.js
+++ public/assets/js/pong/Pong.js
@@ -14,10 +14,11 @@ export default class Pong {
         const PADDLE_WIDTH = 20;
         const PADDLE_HEIGHT = 70;
         const PADDLE_SPEED = 5;
+        const BALL_SPEED = 5;
         const COLOR = "white";
 
         this.#context = document.getElementById("pong").getContext("2d");
-        this.#ball = new Ball(this.#context, BOARD_WIDTH / 2, BOARD_HEIGHT / 2, PADDLE_WIDTH, COLOR);
+        this.#ball = new Ball(this.#context, BOARD_WIDTH / 2, BOARD_HEIGHT / 2, PADDLE_WIDTH, COLOR, BALL_SPEED);
         this.#leftPaddle = new Paddle(this.#context, BOARD_MARGIN, BOARD_HEIGHT / 2 - PADDLE_HEIGHT / 2, PADDLE_WIDTH, PADDLE_HEIGHT, COLOR, PADDLE_SPEED);
         this.#rightPaddle = new Paddle(this.#context, BOARD_WIDTH - PADDLE_WIDTH - BOARD_MARGIN, BOARD_HEIGHT / 2 - PADDLE_HEIGHT / 2, PADDLE_WIDTH, PADDLE_HEIGHT, COLOR, PADDLE_SPEED);
 
