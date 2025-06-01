941d237767618bd5dc7214d8e5928c65da309e5b
Kristof Lißmann <lissmann@email.de>
2025-06-01 22:37:51 +0200

    Update visuals and styles; enhance `Message` readability
    Added an image to the README for better project presentation. Adjusted font sizes in `Message` for improved readability. Updated CSS colors and added a border for a cleaner visual design.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Pong
 
+![Pong](./images/pong.png)
+
 [![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)](https://github.com/prettier/prettier)
 
 The legendary Pong game in object-oriented JavaScript.
--- public/assets/js/pong/drawables/Message.js
+++ public/assets/js/pong/drawables/Message.js
@@ -8,15 +8,16 @@ export default class Message extends Drawable {
     play() {
         super.context.fillStyle = super.color;
         super.context.textAlign = "center";
-        super.context.font = "30px sans-serif";
+        super.context.font = "36px sans-serif";
         super.context.fillText("Press any key to play", super.x, super.y);
     }
 
     winner(player) {
         super.context.fillStyle = super.color;
         super.context.textAlign = "center";
-        super.context.font = "30px sans-serif";
+        super.context.font = "36px sans-serif";
         super.context.fillText(`Player ${player} wins`, super.x, super.y - 30);
+        super.context.font = "24px sans-serif";
         super.context.fillText(
             `Press space to play again`,
             super.x,
